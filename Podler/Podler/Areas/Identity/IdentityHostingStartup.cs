4eee39c7e3080ae64a9adb6416abf45f6923b563
Gabriel Fiuza <34981476+icegabriel@users.noreply.github.com>
2020-10-29 01:44:17 -0300

    Initial

---
+++ Podler/Podler/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Podler.Data;
+using Podler.Models;
+
+[assembly: HostingStartup(typeof(Podler.Areas.Identity.IdentityHostingStartup))]
+namespace Podler.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+
+                var identityConnectionString = context.Configuration.GetConnectionString("IdentityConnection");
+
+                services.AddDbContext<ApplicationIdentityContext>(options =>
+                    options.UseSqlite(identityConnectionString));
+
+                services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<ApplicationIdentityContext>();
+
+                services.AddIdentityServer()
+                    .AddApiAuthorization<ApplicationUser, ApplicationIdentityContext>();
+
+                services.AddAuthentication()
+                    .AddIdentityServerJwt();
+            });
+        }
+    }
+}
---
+++ Podler/Podler/Data/ApplicationIdentityContext.cs
@@ -0,0 +1,17 @@
+﻿using Podler.Models;
+using IdentityServer4.EntityFramework.Options;
+using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace Podler.Data
+{
+    public class ApplicationIdentityContext : ApiAuthorizationDbContext<ApplicationUser>
+    {
+        public ApplicationIdentityContext(
+            DbContextOptions options,
+            IOptions<OperationalStoreOptions> operationalStoreOptions) : base(options, operationalStoreOptions)
+        {
+        }
+    }
+}
